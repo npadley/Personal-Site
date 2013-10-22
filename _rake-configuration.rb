837331c761df11d9ba6964c70f06bb1c6fa51a60
Nick Padley <nick.padley@cd2learning.com>
2013-10-22 10:48:19 -0500

    Additional cleanup for github move

--- _rake-configuration.rb
+++
@@ -1,7 +0,0 @@
-# where the Site is going to live
-$deploy_url     = "http://www.nickpadley.com"
-# the directory on the server (rsync like syntax)
-$deploy_dir     = "nickpadley.com@nickpadley.com:/home/69967/users/.home/domains/nickpadley.com/html"
-# the default post directory
-$post_dir       = "_posts/"
-
